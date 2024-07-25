2a495dc1569fb8b099cd487fac6f442ad539fe0a
Zain Samadi <z.samadi@gmail.com>
2024-07-25 15:34:34 -0700

    Update revCmps.m

--- func/revCmps.m
+++ func/revCmps.m
@@ -1,4 +1,5 @@
 function rcData=revCmps(idata, aLen)    
     rcData=idata(end:-1:1);
+    % making sure erased or extended sequences remain the same
     rcDataCFlag=(rcData>0 & rcData<=aLen);
     rcData(rcDataCFlag)=aLen-rcData(rcDataCFlag)+1;
